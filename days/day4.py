d9660c073c030e5029fc7df376aad15735598621
William Fecteau <william.fecteau@logixoperations.com>
2022-12-04 10:19:41 -0500

    Day 4

---
+++ days/day4.py
@@ -0,0 +1,25 @@
+from utils.aoc_utils import AOCDay, day
+
+
+@day(4)
+class Day4(AOCDay):
+    def common(self):
+        self.pairs = []
+        for line in self.inputData:
+            self.pairs.append([[tuple(map(int, r1.split(
+                '-'))), tuple(map(int, r2.split('-')))] for (r1, r2) in [line.split(',')]][0])
+
+    def isFullyContained(self, a, b, c, d):
+        return (a <= c and b >= d) or (c <= a and d >= b)
+
+    def isOverlapping(self, a, b, c, d):
+        r1 = range(a, b + 1)
+        r2 = range(c, d + 1)
+
+        return len(set(r1).intersection(set(r2))) > 0
+
+    def part1(self):
+        return sum(map(lambda x: self.isFullyContained(*x[0], *x[1]), self.pairs))
+
+    def part2(self):
+        return sum(map(lambda x: self.isOverlapping(*x[0], *x[1]), self.pairs))
